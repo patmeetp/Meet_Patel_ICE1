d54e4adeda2e8575857656a38b05e13f74d5d9bf
patmeetp <146156459+patmeetp@users.noreply.github.com>
2023-10-02 00:10:05 -0400

    Update Card.java

--- src/card/Card.java
+++ src/card/Card.java
@@ -10,8 +10,8 @@ package card;
  * A suit (clubs, hearts, spades, diamonds).
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
- * add your name as a modifier.
  * @author srinivsi
+ * Modified by Meet Patel, 991699476
  */
 public class Card {
 
